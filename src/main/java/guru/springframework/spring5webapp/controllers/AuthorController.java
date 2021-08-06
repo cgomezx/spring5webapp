e54e762937da78c4c4e0ad497a8830f9364b1906
cristobal <cristobalgomez4inf@gmail.com>
2021-08-06 21:29:29 +0200

    assignment 3: display list of authors

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors", this.authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
